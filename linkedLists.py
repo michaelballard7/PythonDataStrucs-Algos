ad5441a02adf12b4216850234e516cbebdcdf8ee
Michael Anthony Ballard <MichaelBallard@Michaels-MBP-2.fios-router.home>
2018-08-31 22:29:57 -0400

    updated link lists

--- linkedLists.py
+++ linkedLists.py
@@ -64,3 +64,22 @@ class LinkedList(object):
         while actualNode is not None:
             print("#d" % actualNode.data)
             actualNode = actualNode.nextNode
+
+    def remove(self,data):
+
+        if self.head is None:
+            return
+
+        self.size = self.size - 1 
+
+        currentNode = self.head
+        previousNode = None
+
+        while currentNode.data != data:
+            previousNode = currentNode
+            currentNode = currentNode.nextNode
+
+        if previousNode is None:
+            self.head = currentNode.nextNode
+        else:
+            previousNode.nextNode = currentNode.nextNode
