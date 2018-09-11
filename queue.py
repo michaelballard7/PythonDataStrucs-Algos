4c097831686ee7508c36c3281e559300884d1424
Michael Anthony Ballard <MichaelBallard@Michaels-MBP.fios-router.home>
2018-09-10 21:40:08 -0400

    comm

--- queue.py
+++ queue.py
@@ -1,6 +1,9 @@
 
+
+
 class Queue:
 
+    # i can use a list as the underlying datatype for my queue
     def __init__(self):
         self.queue = []
 
@@ -10,17 +13,19 @@ class Queue:
     def enqueue(self, data):
         self.queue.append(data)
 
+    # with dequeue I must take off the first element, due FIFO
     def dequeue(self):
         data = self.queue[0]
         del self.queue[0]
         return data
 
+    # as with deueue peeks shows the first element with out removing the first element
     def peek(self):
         return self.queue[0]
 
+
     def sizeQueue(self):
         return len(self.queue)
-
 
 queue = Queue()
 
